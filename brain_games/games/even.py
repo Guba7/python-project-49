6989650253927f3a11c70e7bb49028a2496387ff
egorcrypt7 <kaptsof@gmail.com>
2023-03-31 15:19:04 +0300

    Fix

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,8 +4,8 @@ import random
 
 def generate_question_answer():
     rnd_numb = random.randint(1, 100)
-    question_1 = ("Answer \"yes\" if the number is even, "
-                  + "otherwise answer \"no\".")
+    question_1 = ("Answer \"yes\" if the number is even," \ 
+                  " otherwise answer \"no\".")
     question_2 = (f'Question: {rnd_numb}')
     if rnd_numb % 2 == 0:
         correct_answer = 'yes'
